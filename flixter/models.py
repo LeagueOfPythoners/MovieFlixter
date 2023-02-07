5175f98bbc2ebd8c9c10adf1e391a0889b623e15
GreyWoulf <greywoulf88@outlook.com>
2023-02-06 19:49:45 -0500

    added class data model

--- flixter/models.py
+++ flixter/models.py
@@ -1,3 +1,20 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+
+CHARS_MAX_LENGTH: int = 150
+class Movie(models.Model):
+    # movie model class (creates a table named model into our database)
+    
+    name = models.CharField(max_length=CHARS_MAX_LENGTH, blank=True)
+    #adds column named name into movie table with no more than 150 characters
+    
+    description = models.TextField(blank=True, null=True)
+    # movie can be created without specifying a value for the description
+    
+    data_created = models.DateTimeField(auto_now_add=True)
+    #creation date is automatically set
+    
+    
+    
