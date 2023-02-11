776d34112ffba75141caf72264855654d9be0dd6
GreyWoulf <greywoulf88@outlook.com>
2023-02-11 14:05:50 -0500

    Added admin site categories

--- flixter/admin.py
+++ flixter/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from flixter.models import Movie
+from flixter.models import Category
+from flixter.models import Tag
 # Register your models here.
+
+admin.site.register(Movie)
+admin.site.register(Category)
+admin.site.register(Tag)
